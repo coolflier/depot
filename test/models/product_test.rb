87c54bd1a0f554649a00011dc68190e975061c71
coolflier <rag.anand@gmail.com>
2015-01-03 17:48:56 +0530

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,7 +11,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-  	product = Product.new (title: "Crazy Testing",
+  	product = Product.new(title: "Crazy Testing",
   		description: "Crazy Testing Mechanism", image_url: "crazy.png")
   	product.price = -1
   	assert product.invalid?
@@ -24,8 +24,48 @@ class ProductTest < ActiveSupport::TestCase
   		product.errors[:price]
 
   	product.price = 1
-  	assert product.valid?
+  	assert product.valid?  	
+  end
+
+  # test "should not create products with the duplicate title" do
+  #  	product = Product.new(title: products(:three).title,
+  #   	description: "Duplicate Title",
+  #   	price: 19.4,
+  #   	image_url: "razy.gif")
+  #   assert product.invalid?
+  #   assert_equal ["has already been taken"], product.errors[:title]
+    
+  # end
+
+  test "should not create products with the duplicate title" do
+    product = Product.new(title: products(:three).title,
+      description: "Duplicate Title",
+      price: 19.4,
+      image_url: "razy.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  def new_product(image_url)
+  	Product.new(title: "My Book Title",
+  		description: "Description",
+  		price: 19.95,
+  		image_url: image_url)
+  end
+
+  test "Image URL" do
+  	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.PNG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+  	bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+  	ok.each do |name|
+  		assert new_product(name).valid?, "#{name} should be valid"
+  	end
+
+  	bad.each do |name|
+  		assert new_product(name).invalid?, "#{name} should be invalid"
+  	end
   	
+
   end
 
 end
